dc222254044e046175fc8ceeee605a5a3c079524
Dmitry Marchenko <>
2026-05-02 08:47:51 +0300

    setup the basic classes

---
+++ Source/ProjectNorth/Private/Player/PNPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/PNCharacter.h"
+#include "PNPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APNPlayerCharacter : public APNCharacter
+{
+	GENERATED_BODY()
+	
+};
